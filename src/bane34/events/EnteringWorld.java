e63eba756fb96b5a885b587ba6e50f5804bb4e3a
bane34 <bane34.42@gmail.com>
2021-01-03 18:38:47 +0100

    now when you enter you odnt teleport to an specific location

--- src/bane34/events/EnteringWorld.java
+++ src/bane34/events/EnteringWorld.java
@@ -1,17 +1,22 @@
 package bane34.events;
 
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
-public class EnteringWorld implements Listener{
-	
+public class EnteringWorld implements Listener {
+
 	@EventHandler
 	public void onPlayerEnter(PlayerJoinEvent event) {
 		Player player = event.getPlayer();
+		World world = player.getWorld();
+		Location location = player.getLocation();
 		
-		player.getWorld().strikeLightningEffect(player.getLocation());
+		world.strikeLightningEffect(location);
 		return;
 	}
+	
 }
