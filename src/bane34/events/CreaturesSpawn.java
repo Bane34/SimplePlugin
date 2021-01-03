eb71f81a60e8314912fcf9b1a5d72c821abdd342
bane34 <bane34.42@gmail.com>
2021-01-03 18:40:44 +0100

    now the creepers only spawn electrified and the zombies with full set of diamond

---
+++ src/bane34/events/CreaturesSpawn.java
@@ -0,0 +1,34 @@
+package bane34.events;
+
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Creeper;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Zombie;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class CreaturesSpawn implements Listener {
+	
+	@EventHandler
+	public void creatureSpawn(CreatureSpawnEvent event) {
+		if(event.getEntityType() == EntityType.CREEPER) {
+			Creeper creeper = (Creeper) event.getEntity();
+			creeper.setPowered(true);
+		}
+		
+		if(event.getEntityType() == EntityType.ZOMBIE) {
+			Zombie zombie = (Zombie) event.getEntity();
+			zombie.getEquipment().setHelmet(new ItemStack(Material.DIAMOND_HELMET));
+			zombie.getEquipment().setLeggings(new ItemStack(Material.DIAMOND_LEGGINGS));
+			zombie.getEquipment().setChestplate(new ItemStack(Material.DIAMOND_CHESTPLATE));
+			zombie.getEquipment().setBoots(new ItemStack(Material.DIAMOND_BOOTS));
+			
+			ItemStack sharpnessSword = new ItemStack(Material.DIAMOND_SWORD);
+			sharpnessSword.addEnchantment(Enchantment.DAMAGE_ALL, 5);
+		}
+	}
+
+}
