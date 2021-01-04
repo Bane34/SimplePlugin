b13305acd6afe90efc4763c3fd549a5d85c38a34
bane34 <bane34.42@gmail.com>
2021-01-04 11:38:14 +0100

    welcome mensagge when a player enters the server

--- src/bane34/events/EnteringWorld.java
+++ src/bane34/events/EnteringWorld.java
@@ -2,21 +2,37 @@ package bane34.events;
 
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import bane34.main.SimplePlugin;
+
 public class EnteringWorld implements Listener {
+	private SimplePlugin plugin;
+	
+	public EnteringWorld(SimplePlugin plugin) {
+		this.plugin = plugin;
+	}
 
 	@EventHandler
-	public void onPlayerEnter(PlayerJoinEvent event) {
+	public void onPlayerEnterStrikeLightning(PlayerJoinEvent event) {
 		Player player = event.getPlayer();
 		World world = player.getWorld();
 		Location location = player.getLocation();
 		
+		FileConfiguration config = plugin.getConfig();
+		String path = "Config.welcome-message";
+		
+		
+		if(config.getString(path).equals("true")) {
+			String text = "Config.welcome-message-text";
+			player.sendMessage(config.getString(text));
+		}
+				
 		world.strikeLightningEffect(location);
-		return;
+		
 	}
-	
 }
