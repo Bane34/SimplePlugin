6b315f7b97f9fa9df7ad50372db997ba6aff7cf4
bane34 <bane34.42@gmail.com>
2021-01-06 21:12:17 +0100

    fixed the name of the deathEvent

--- src/bane34/main/SimplePlugin.java
+++ src/bane34/main/SimplePlugin.java
@@ -9,9 +9,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import bane34.commands.MainCommand;
 import bane34.commands.StrikeLightning;
-import bane34.events.ChickenDead;
 import bane34.events.CreaturesSpawn;
 import bane34.events.EnteringWorld;
+import bane34.events.EntityDeath;
 import bane34.events.ThrowingThings;
 
 public class SimplePlugin extends JavaPlugin{
@@ -44,11 +44,11 @@ public class SimplePlugin extends JavaPlugin{
 		pm.registerEvents(new EnteringWorld(this), this);
 		pm.registerEvents(new ThrowingThings(), this);
 		pm.registerEvents(new CreaturesSpawn(), this);
-		pm.registerEvents(new ChickenDead(), this);
+		pm.registerEvents(new EntityDeath(), this);
 	}
 	
 	public void registerConfig() {
-		File config = new File(this.getDataFolder(),"config.yml");
+		File config = new File(this.getDataFolder(), "config.yml");
 		configPath = config.getPath();
 		
 		if(!config.exists()) {
